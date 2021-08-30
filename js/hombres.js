1d7358f9f43838cd1f6f6a7083fff9dc13222b60
Nicolas Minali <nicominali@hotmail.com>
2021-08-30 18:19:37 -0300

    boton favorito en proceso

---
+++ js/hombres.js
@@ -0,0 +1,6 @@
+function encender(){
+    document.querySelector("img").src ="../assets/img/estrella-on.png";
+}
+function apagar(){
+    document.querySelector("img").src ="../assets/img/estrella-off.png";
+}
