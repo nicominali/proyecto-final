732f0d44f7ff0f4e7db0be91dbb786eaeadb2233
Nicolas Minali <nicominali@hotmail.com>
2021-09-14 14:38:54 -0300

    barra de fav

--- js/hombres.js
+++ js/hombres.js
@@ -63,7 +63,9 @@ const ropas = [
 ]
 //const estrella = document.querySelector(".estrella");
 
+let ropasFavoritas = []; 
 const contenedorRopa = document.querySelector(".container-ropa");
+//const mostrarRopas = document.querySelector(".container-ropa")
 
 document.addEventListener('DOMContentLoaded',() => {
     
@@ -73,7 +75,7 @@ document.addEventListener('DOMContentLoaded',() => {
 function mostrarRopas(){
     ropas.forEach((ropa) => {
       
-      const div = document.createElement("div");
+      /*const div = document.createElement("div");
       div.classList.add("zoom");
      
       const imagen = document.createElement("img");
@@ -84,26 +86,106 @@ function mostrarRopas(){
      // nombreRopa.classList.add("nombre-ropa")
       nombre.textContent = ropa.name
 
-      const favoritos = document.createElement("img");
+      const favoritos = document.createElement("button");
       favoritos.src ="../assets/img/estrella-off.png"
       favoritos.classList.add ("estrella");
+      favoritos.onclick = () => {
+        agregarAFavorito(ropa.id);
+      };
 
       div.appendChild(imagen);
       div.appendChild(nombre);
       div.appendChild(favoritos)
     
-     contenedorRopa.appendChild(div); 
-      
+     contenedorRopa.appendChild(div); */
+     
+     contenedorRopa.innerHTML += `
+     <section class="container-ropa" data-aos="fade-right">
+        <div class="row">
+            <div class="col-lg-4 col-xs-12">
+               <div class="zoom">
+                <img class="estrella" src="../assets/img/estrella-off.png" alt="estrella-off" width="45px" height="45px" onclick="encender()"ondblclick="apagar()">
+                <img class="imgRopa" src="../assets/imagenes-productos-H/buzo-cinco.jpeg" alt="buzo-cinco" width="350px" height="350px">
+               <div class="textoFoto"> Buzo deportivo</div>
+               <div class="textoFoto">la secleccion</div>
+               </div><br>
+               <div class="zoom">
+                <img class="estrella" src="../assets/img/estrella-off.png" alt="estrella-off" width="45px" height="45px" onclick="encender()"ondblclick="apagar()">
+                <img class="imgRopa" src="../assets/imagenes-productos-H/buzo-tres.jpeg" alt="buzo-tres" width="350px" height="350px">
+                <div class="textoFoto">buzo brazos militares</div>
+               </div><br>
+               <div class="zoom">
+                <img class="estrella" src="../assets/img/estrella-off.png" alt="estrella-off" width="45px" height="45px" onclick="encender()"ondblclick="apagar()">
+                <img  src="../assets/imagenes-productos-H/conjunto-cuatro.jpeg" alt="conjuto-cuatro" width="350px" height="350px">
+                <div>conjunto deportivo negro</div>
+               </div><br>
+               <div class="zoom">
+                <img class="estrella" src="../assets/img/estrella-off.png" alt="estrella-off" width="45px" height="45px"> 
+               <img src="../assets/imagenes-productos-H/pantalon-seis.jpeg" alt="pantalon-seis" width="350px" height="350px">
+               <div>pantalon cargo</div>
+               <div>crema</div>
+               </div>
+            </div>
+            <div class="col-lg-4 col-xs-12">
+                <div class="zoom">
+                <img class="estrella" src="../assets/img/estrella-off.png" alt="estrella-off" width="45px" height="45px">
+                <img src="../assets/imagenes-productos-H/remera-cuatro.jpeg" alt="remera-cuatro" width="350px" height="350px">
+                <div>remera con estampa</div>
+                </div><br>
+                <div class="zoom">
+                <img class="estrella" src="../assets/img/estrella-off.png" alt="estrella-off" width="45px" height="45px">
+                <img src="../assets/imagenes-productos-H/remera-uno.jpeg" alt="remera-uno" width="350px" height="350px">
+                <div>remera estampa</div>
+                <div>pato lucas</div>
+                </div><br>
+                <div class="zoom"><img class="estrella" src="../assets/img/estrella-off.png" alt="estrella-off" width="45px" height="45px">
+                <img src="../assets/imagenes-productos-H/remera-dos.jpeg" alt="remera-dos" width="350px" height="350px">
+                <div>remera lisa blanca</div>
+                </div><br>
+                <div class="zoom">
+                <img class="estrella" src="../assets/img/estrella-off.png" alt="estrella-off" width="45px" height="45px">
+                <img  src="../assets/imagenes-productos-H/remera-siete.jpeg" alt="remera-siete" width="350px" height="350px">
+                <div>remera advisory</div>
+                </div><br>  
+            </div>
+            <div class="col-lg-4 col-xs-12">
+                <div class="zoom">
+                <img class="estrella" src="../assets/img/estrella-off.png" alt="estrella-off" width="45px" height="45px">    
+                <img src="../assets/imagenes-productos-H/buzo-ocho.jpeg" alt="buzo-ocho" width="350px" height="370px">
+                <div>campera negra</div>
+                </div><br>
+                <div class="zoom">
+                <img class="estrella" src="../assets/img/estrella-off.png" alt="estrella-off" width="45px" height="45px">    
+                <img  src="../assets/imagenes-productos-H/buzo-trece.jpeg" alt="buzo-trece" width="350px" height="350px">
+                <div>campera hentaimob</div>
+                </div><br>
+                <div class="zoom">
+                <img class="estrella" src="../assets/img/estrella-off.png" alt="estrella-off" width="45px" height="45px">
+                <img src="../assets/imagenes-productos-H/buzo-diez.jpeg" alt="buzo-diez" width="350px" height="350px">
+                <div>buzo bad batch</div>
+                </div><br>
+                <div class="zoom">
+                <img class="estrella" src="../assets/img/estrella-off.png" alt="estrella-off" width="45px" height="45px"> 
+                <img src="../assets/imagenes-productos-H/buzo-doce.jpeg" alt="buzo-doce" width="350px" height="350px">
+                <div>buzo negro jordan</div>
+                </div><br>
+            </div>
+        </div>  
+    </section><br></br>`
+    
+    //contenedorRopa.appendChild()
+    
     })
 }
-/*
+
 function agregarAFavorito(id){
     const ropaFavorita = ropas.find (ropa => {
-        return ropa.id === id
+        return ropa.id === id;
     })
-}
-console.log(ropaFavorita);
-function encender(){
+    ropasFavoritas.push(ropaFavorita)
+    console.log(ropasFavoritas);
+  }
+/*function encender(){
     document.querySelector("img").src ="../assets/img/estrella-on.png";
 }
 function apagar(){
