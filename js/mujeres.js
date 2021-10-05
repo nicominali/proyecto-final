3b79d6fa551d5cbf7a1d5e4e5b16ac341ade7cc0
Nicolas Minali <nicominali@hotmail.com>
2021-10-05 00:44:30 -0300

    boton nocturno

--- js/mujeres.js
+++ js/mujeres.js
@@ -1,108 +1,3 @@
-/*function ajax() {
-    const http = new XMLHttpRequest ();
-    const url = '/pages/m-segunda-pagina.html';
-
-    
-    http.onreadystatechange = function (){
-        if(this.readyState === 4 && this.status === 200){
-            console.log(this.response);
-            document.getElementById("masRopa").innerHTML = this.responseText;
-        }
-    }
-    http.open("GET" , url);
-    http.send();
-}
-
-document.getElementById("boton").addEventListener('click' function (){
-    ajax();
-});
-
-// carrito de compras
-
-const addToShoppingCartButtons = document.querySelectorAll('.botonCompra')
-addToShoppingCartButtons.forEach((addToCartButton) => {
- addToCartButton.addEventListener('click', addToCartClicked);
-});
-
-
-
-const shoppingCartItemsContainer = document.getElementsByClassName('.shoppingCartItemsContainer');
-console.log(shoppingCartItemsContainer)
-
-function addToCartClicked(event) {
-    const button = event.target;
-    const item = button.closest('.item');
-    
-    console.log(item)
-
-    const itemPrice = document.getElementsByClassName ('.item-price');
-    const itemImage = document.getElementsByClassName ('.item-image');
-    const itemTitle = document.getElementsByClassName ('.item-title');
-  
-   // console.log(itemTitle, itemPrice, itemImage);
-   addItemToShoppingCart(itemTitle, itemPrice, itemImage);
-
-
-}
-const shoppingCartRow = document.createElement('div');
-const shoppingCartContent = `
-<div class="row shoppingCartItem">
-      <div class="col-6">
-          <div class="shopping-cart-item d-flex align-items-center h-100 border-bottom pb-2 pt-3">
-              <img src=${itemImage} class="shopping-cart-image">
-              <h6 class="shopping-cart-item-title shoppingCartItemTitle text-truncate ml-3 mb-0">${itemTitle}</h6>
-          </div>
-      </div>
-      <div class="col-2">
-          <div class="shopping-cart-price d-flex align-items-center h-100 border-bottom pb-2 pt-3">
-              <p class="item-price mb-0 shoppingCartItemPrice">${itemPrice}</p>
-          </div>
-      </div>
-      <div class="col-4">
-          <div
-              class="shopping-cart-quantity d-flex justify-content-between align-items-center h-100 border-bottom pb-2 pt-3">
-              <input class="shopping-cart-quantity-input shoppingCartItemQuantity" type="number"
-                  value="1">
-              <button class="btn btn-danger buttonDelete" type="button">X</button>
-          </div>
-      </div>
-  </div>`;
-shoppingCartRow.innerHTML = shoppingCartContent;
-shoppingCartItemsContainer.append(shoppingCartRow);
-shoppingCartRow
-  .querySelector('.buttonDelete')
-  .addEventListener('click', removeShoppingCartItem);
-
-shoppingCartRow
-  .querySelector('.shoppingCartItemQuantity')
-  .addEventListener('change', quantityChanged);
-
-updateShoppingCartTotal();
-
-
-function updateShoppingCartTotal(){
-    let total = 0;
-    const shoppingCartTotal = document.querySelectorAll('.shoppingCartTotal'); 
-    
-    const shoppingCartItems = document.querySelectorAll('.shoppingCartItem')
-    
-    shoppingCartItems.forEach((shoppingCartItem) =>{
-        const shoppingCartContentItemPriceElement = shoppingCartItem.querySelectorAll('.shoppingCartItemPrice');
-        
-        const shoppingCartItemPrice = Number (shoppingCartContentItemPriceElement.textContent.replace('$',''));
-        
-        const shoppingCartItemQuantityElement = shoppingCartItem.querySelector('.shoppingCartItemQuantity');
-        
-        const shoppingCartItemQuantity = Number (shoppingCartItemQuantityElement.value);
-        
-        total = total + shoppingCartItemPrice * shoppingCartItemQuantity;
-        
-        shoppingCartTotal.innerHTML =`${total.toFixed(2)}$`;
-
-
-    });
-}
-*/
 const addToShoppingCartButtons = document.querySelectorAll('.botonCompra');
 addToShoppingCartButtons.forEach((addToCartButton) => {
   addToCartButton.addEventListener('click', addToCartClicked);
@@ -221,3 +116,10 @@ function comprarButtonClicked() {
   shoppingCartItemsContainer.innerHTML = '';
   updateShoppingCartTotal();
 }
+ 
+const btnSwitch = document.querySelector('#switch')
+
+btnSwitch.addEventListener('click', () => {
+  document.body.classList.toggle('dark');
+  btnSwitch.classList.toggle('active')
+});
